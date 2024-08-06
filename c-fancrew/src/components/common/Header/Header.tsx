b221d604fb582c3454ad5b8315795b1b2b8405c3
Hiroto N <hiroto4455656@gmail.com>
2024-08-07 06:20:18 +0900

    fancrew web init

---
+++ c-fancrew/src/components/common/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#f3f3f3]'>
+      <ul className='mx-auto flex py-1.5 max-w-[1024px] justify-end flex-wrap '>
+        <li>test1</li>
+        <li>test2</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
