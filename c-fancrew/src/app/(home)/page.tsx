d48c03c34e5f1dcabad2f0dee2203632f37af4ca
Hiroto N <hiroto4455656@gmail.com>
2024-08-09 20:34:36 +0900

    fancrew-header part

---
+++ c-fancrew/src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import GuestBar from "@/components/common/Header/GuestBar/GuestBar";
+import DesktopMainPic from "@/components/home/desktop/DesktopMainPic";
+
+export default function Home() {
+  return (
+    <>
+      <GuestBar/>
+      <DesktopMainPic/>
+
+    </>
+  )
+}
--- c-fancrew/src/components/common/Footer/Footer.tsx
+++ c-fancrew/src/components/common/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='pt-0 mx-auto p-8 max-w-[1024px] border-t-[10px] border-t-[#f3f3f3] text-center'>
+      Footer is under construction
     </div>
   )
 }
