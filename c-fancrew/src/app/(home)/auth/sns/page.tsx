48d90d17392aa7748d91a1469e0f3bd120ae13c4
Hiroto N <hiroto4455656@gmail.com>
2024-08-18 06:46:03 +0900

    page router import fix

--- c-fancrew/src/app/(home)/auth/sns/page.tsx
+++ c-fancrew/src/app/(home)/auth/sns/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { FormState, verifyOAuthCallback } from "@/actions/account";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useFormState } from "react-dom";
 
 const AuthCallback = () => {
+  console.log("useRouter前");
   const router = useRouter();
+  console.log("useRouter後");
   const [error, setError] = useState<string | null>(null);
   const initialState: FormState = { error: "" };
   const [state, formAction] = useFormState(verifyOAuthCallback, initialState);
@@ -23,20 +25,20 @@ const AuthCallback = () => {
 
       const form = document.getElementById('callbackAction') as HTMLFormElement;
       if (form) {
-        form.submit(); // フォームを自動的に送信
+        // form.submit(); // フォームを自動的に送信
       }
     };
 
     if (router.isReady) {
-      handleOAuthCallback();
+      // handleOAuthCallback();
     }
-  }, [router]);
+  }, []);
 
   return (
     <form id="callbackAction" action={formAction}>
-      <div className="mt-[1.5rem] mr-auto mb-[3.5rem] flex max-w-5xl justify-between gap-7 w-full">
+      <div className="mt-[1.5rem] m-auto mb-[3.5rem] flex max-w-5xl justify-between gap-7 w-full">
         <div className="w-full flex p-[20vh_0] text-center items-center flex-col justify-center bg-[#f29100]">
-          <span className="text-white block text-[4rem] font-bold mb-[4rem]">
+          <span className="text-white block text-[3rem] font-bold mb-[4rem]">
             認証中
           </span>
         </div>
