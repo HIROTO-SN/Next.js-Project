39348529984b3ca6a89e44d78b75140354608d1e
Hiroto N <hiroto4455656@gmail.com>
2024-09-07 06:37:09 +0900

    current page status bar implemented

--- c-fancrew/src/components/common/PageConfirm/Arrow.tsx
+++ c-fancrew/src/components/common/PageConfirm/Arrow.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Arrow = () => {
   return (
-    <div>Arrow</div>
+    <span className='w-[0.3rem] mr-[0.3rem] text-[1rem] leading-3'>
+      <div className='relative'>
+        <div className='max-w-full overflow-hidden relative box-border my-0 mx-auto block max-h-full'>
+          <div className='box-border block max-w-full'></div>
+          <img alt="arrow" src="/obj/next-black.svg" decoding="async" data-nimg="intrinsic"/>
+        </div>
+      </div>
+    </span>
   )
 }
 
--- c-fancrew/src/utils/commonUtils.ts
+++ c-fancrew/src/utils/commonUtils.ts
@@ -7,3 +7,12 @@ import crypto from 'crypto';
 export const generateUUID = () => {
   return crypto.randomBytes(16).toString('hex');
 };
+
+/**
+* URLの最後のスラッシュ以降の文字列を取得する関数
+ * @param {string} url - 対象のURL
+ * @return {string} - スラッシュ以降の文字列
+ */
+export const getLastSegmentUrl = (url: string): string => {
+  return url.substring(url.lastIndexOf('/') + 1);
+};
