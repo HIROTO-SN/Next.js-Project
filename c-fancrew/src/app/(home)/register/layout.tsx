53c557d3ec804f03464e6279221c4276b9c3a11e
Hiroto N <hiroto4455656@gmail.com>
2024-10-18 04:38:46 +0900

    re-render the layout.tsx when the pasthname changes

--- c-fancrew/src/app/(home)/register/layout.tsx
+++ c-fancrew/src/app/(home)/register/layout.tsx
@@ -3,12 +3,25 @@
 import PageConfirm from "@/components/common/PageConfirm/PageConfirm";
 import { RegisterAccountProvider } from "@/contexts/RegisterContext/RegisterAccount";
 import { getLastSegmentUrl } from "@/utils/commonUtils";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const currentPath = window.location.pathname;
+  const pathname = usePathname();
+  const [currentUrl, setCurrentUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    // クライアントで実行する
+    if (pathname) {
+      setCurrentUrl(getLastSegmentUrl(pathname));
+    }
+  }, [pathname]);
+
+  if (!currentUrl) return null;
+
   return (
     <>
-      <PageConfirm currentUrl={getLastSegmentUrl(currentPath)}/>
+      <PageConfirm currentUrl={currentUrl}/>
       <section className="pt-[2rem] pr-0 pb-[4rem] m-auto max-w-[768px] min-h-[80vh]">
         <RegisterAccountProvider>
           {children}
